15e4202b31fb0d72c3535bd97c102691ce285c98
Jem <jem@dercetech.com>
2019-03-24 01:07:20 +0100

    Initial THREE.js setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,19 @@ const path = require('path');
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
 const config = require('./env/dev')();
+
+gulp.task('engine:scss', () =>
+  gulp
+    .src('./engine/scss/styles.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.folders.public))
+);
 
 gulp.task('engine:concat', () =>
   gulp
@@ -16,6 +27,7 @@ gulp.task('engine:concat', () =>
 
 gulp.task('engine:watch', () => {
   gulp.watch('engine/**/*.js', gulp.series(['engine:concat']));
+  gulp.watch('engine/**/*.js', gulp.series(['engine:scss']));
 });
 
-gulp.task('dev', gulp.parallel(['engine:concat', 'engine:watch'], done => done()));
+gulp.task('dev', gulp.parallel(['engine:concat', 'engine:scss', 'engine:watch'], done => done()));
